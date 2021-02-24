b6be4fd7c396bd50778a78ff46904dcd6f9e2005
ntiutiunnyk <vfkznrj2411>
2021-02-24 16:53:03 +0200

    fix

--- LuckyTicket/Program.cs
+++ LuckyTicket/Program.cs
@@ -1,12 +1,65 @@
-﻿using System;
+using System;
+
 
 namespace LuckyTicket
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
-            Ticket.getLuckyTicket();
+            getLuckyTicket();
+        }
+
+        public static int countNumbersSum(string numString)
+        {
+            int sum = 0;
+            for (int i = 0; i < numString.Length; i++)
+            {
+                sum += numString[i];
+            }
+            return sum;
+        }
+
+        public static void getLuckyTicket()
+        {
+            Console.WriteLine("Enter a ticket number to check if it is lucky or '0' to exit from the program");
+            string ticket = Console.ReadLine();
+            int ticketLength = ticket.Length;
+            if (ticket == "0")
+            {
+                Console.WriteLine("The program is ended");
+                return;
+            }
+            else if (ticketLength > 8 || ticketLength < 4)
+            {
+                Console.WriteLine("The length of your ticket number is out of range from 4 to 8 digits");
+                getLuckyTicket();
+            }
+            int num;
+            bool isConverted = Int32.TryParse(ticket, out num);
+            if (isConverted)
+            {
+                if (ticketLength % 2 != 0)
+                {
+                    ticket = ticket.Insert(0, "0");
+                    ticketLength = ticket.Length;
+                }
+                string part1 = ticket.Substring(0, ticketLength / 2);
+                string part2 = ticket.Substring(ticketLength / 2);
+                if (countNumbersSum(part1) == countNumbersSum(part2))
+                {
+                    Console.WriteLine($"Your ticket with number {ticket} is lucky");
+                }
+                else
+                {
+                    Console.WriteLine($"Your ticket with number {ticket} is not lucky");
+                }
+            }
+            else
+            {
+                Console.WriteLine($"Your ticket with number {ticket} can't b converted into number");
+            }
+            getLuckyTicket();
         }
     }
 }
--- run.bat
+++ run.bat
@@ -1,2 +1,3 @@
 dotnet build
 dotnet run --project "./LuckyTicket/LuckyTicket.csproj"
+pause
